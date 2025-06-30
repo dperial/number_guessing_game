a285b738cefd100455103b528361f991aa55a683
Perial Dupont Kuaguim N <dperial44@gmail.com>
2025-06-30 16:23:40 +0000

    feat: add the logic for the function guess_number_logic

--- number_guess.sh
+++ number_guess.sh
@@ -8,7 +8,9 @@ MAIN_MENU(){
   USER_DETAIL
   # Capture the user guess number
   GET_USER_GUESS
-  echo "Your guess was: $GUESS_NUMBER"
+  # echo "Your guess was: $GUESS_NUMBER"
+  # Call of Guess number logic function
+  GUESS_NUMBER_LOGIC
 }
 
 USER_DETAIL(){
@@ -30,4 +32,26 @@ GET_USER_GUESS(){
   echo -e "Guess the secret number between 1 and 1000:"
   read GUESS_NUMBER
 }
+GUESS_NUMBER_LOGIC(){
+  NUMBER_OF_GUESSES=1
+
+  while [[ "$GUESS_NUMBER" != "$secret_number" ]]
+  do
+    if ! [[ "$GUESS_NUMBER" =~ ^[0-9]+$ ]]
+    then
+      echo "That is not an integer, guess again:"
+    elif [[ "$GUESS_NUMBER" -gt "$secret_number" ]]
+    then
+      echo "$secret_number is lower than that $GUESS_NUMBER, guess again:"
+    else
+      echo "$secret_number is higher than that $GUESS_NUMBER, guess again:"
+    fi
+    read GUESS_NUMBER
+    ((NUMBER_OF_GUESSES++))
+  done
+  # Insert the final argument into the table games
+  $PSQL "INSERT INTO games(secret_number, player_id, guess_count) VALUES($secret_number, $USER_ID, $NUMBER_OF_GUESSES);"
+  echo "Welcome back, $USERNAME!, You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $secret_number. Nice job!"
+}
+
 MAIN_MENU
