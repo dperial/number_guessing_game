315918c3ae37bfc7b080ec28c52b58f00de3e8b0
Perial Dupont Kuaguim N <dperial44@gmail.com>
2025-06-30 15:22:41 +0000

    feat: new user added into the db

--- number_guess.sh
+++ number_guess.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-# Connect to the db
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=postgres -t --no-align -c"
 
 echo -e "\n~~~~~ A Number Guessing Game ~~~~~\n"
 
 MAIN_MENU(){
-  # Generate the secret number
   secret_number=$(( RANDOM % 1000 + 1 ))
-  echo "Secret number generated: $secret_number"
+  USER_DETAIL
 }
 
+USER_DETAIL(){
+  echo -e "\nEnter your username:"
+  read USERNAME
+  USERNAME=$(echo "$USERNAME" | sed "s/'/''/g")
+  
+  USER_ID=$($PSQL "SELECT player_id FROM players WHERE username='$USERNAME';")
+  
+  if [[ -z $USER_ID ]]
+  then
+    $PSQL "INSERT INTO players(username) VALUES('$USERNAME');"
+    echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+  else
+    echo -e "\nWelcome back"
+  fi
+}
 MAIN_MENU
