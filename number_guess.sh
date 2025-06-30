4acb7afd7bd4a30644d7299e184e43e2ed182046
Perial Dupont Kuaguim N <dperial44@gmail.com>
2025-06-30 15:00:30 +0000

    feat: generate a ramdon number between 1 and 1000

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+# Connect to the db
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+echo -e "\n~~~~~ A Number Guessing Game ~~~~~\n"
+
+MAIN_MENU(){
+  # Generate the secret number
+  secret_number=$(( RANDOM % 1000 + 1 ))
+  echo "Secret number generated: $secret_number"
+}
+
+MAIN_MENU
